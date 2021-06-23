eb6bf6c5a7d49a67855f94581cd228d247d7e2c2
gunasekharravilla <gunasekharravilla156@gmail.com>
2021-06-23 21:30:07 +0530

    loading events from backend

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,40 @@
-import React from "react";
-import data from "../data";
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 import Event from "../components/Event";
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 function HomeScreen() {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  useEffect(() => {
+    const fecthData = async () => {
+       try {
+        setLoading(true);
+        const { data } = await axios.get('/api/events');
+        setLoading(false);
+        setEvents(data);
+      } catch (err) {
+        setError(err.message);
+        setLoading(false);
+      }
+    };
+    fecthData();
+  }, []);
   return (
     <div>
-      <div className="row center">
-        {data.events.map((event) => (
-          <Event key={event._id} event={event}></Event>
-        ))}
-      </div>
+     {loading ? (
+        <LoadingBox></LoadingBox>
+      ) : error ? (
+        <MessageBox variant="danger">{error}</MessageBox>
+      ) : (
+        <div className="row center">
+          {events.map((event) => (
+            <Event key={event._id} event={event}></Event>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
