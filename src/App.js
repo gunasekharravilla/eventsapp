b82817a3fba83e2fba9854dda45c8ce041cd1f30
gunasekharravilla <gunasekharravilla156@gmail.com>
2021-06-25 20:12:53 +0530

    create button issue solved ✌

--- src/App.js
+++ src/App.js
@@ -1,59 +1,8 @@
-import React, { useEffect } from "react";
-import Axios from "axios";
-import { useDispatch, useSelector } from "react-redux";
 import { BrowserRouter, Route } from "react-router-dom";
 import HomeScreen from "./screens/HomeScreen";
-import LoadingBox from "./components/LoadingBox";
-import MessageBox from "./components/MessageBox";
 import EventEditScreen from "./screens/EventEditScreen";
-//import { useHistory } from "react-router-dom";
-//import { history } from "history";
 
-function App(props) {
-  const EVENT_CREATE_REQUEST = "EVENT_CREATE_REQUEST";
-  const EVENT_CREATE_SUCCESS = "EVENT_CREATE_SUCCESS";
-  const EVENT_CREATE_FAIL = "EVENT_CREATE_FAIL";
-  const EVENT_CREATE_RESET = "EVENT_CREATE_RESET";
-  //const history = useHistory();
-
-  const createEvent = () => async (dispatch) => {
-    dispatch({ type: EVENT_CREATE_REQUEST });
-    try {
-      const { data } = await Axios.post("/api/events", {});
-      dispatch({
-        type: EVENT_CREATE_SUCCESS,
-        payload: data.event,
-      });
-    } catch (error) {
-      const message =
-        error.response && error.response.data.message
-          ? error.response.data.message
-          : error.message;
-      dispatch({ type: EVENT_CREATE_FAIL, payload: message });
-    }
-  };
-
-  const eventCreate = useSelector((state) => state.eventCreate);
-  const {
-    loading: loadingCreate,
-    error: errorCreate,
-    success: successCreate,
-    event: createdEvent,
-  } = eventCreate;
-
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    if (successCreate) {
-      dispatch({ type: EVENT_CREATE_RESET });
-      props.history.push(`/event/${createdEvent._id}/edit`);
-    }
-  }, [createdEvent, dispatch, props.history, successCreate]);
-
-  const createHandler = () => {
-    dispatch(createEvent());
-  };
-
+function App() {
   return (
     <BrowserRouter>
       <div className="grid-container">
@@ -66,15 +15,6 @@ function App(props) {
               />
             </a>
           </div>
-          <div className="row">
-            <button type="button" className="primary" onClick={createHandler}>
-              Create Event
-            </button>
-          </div>
-          {loadingCreate && <LoadingBox></LoadingBox>}
-          {errorCreate && (
-            <MessageBox variant="danger">{errorCreate}</MessageBox>
-          )}
         </header>
         <main>
           <Route path="/" component={HomeScreen} exact></Route>
--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
-import axios from "axios";
+import { useDispatch, useSelector } from "react-redux";
+import Axios from "axios";
 import Event from "../components/Event";
 import LoadingBox from "../components/LoadingBox";
 import MessageBox from "../components/MessageBox";
 
-function HomeScreen() {
+function HomeScreen(props) {
   const [events, setEvents] = useState([]);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
@@ -12,7 +13,7 @@ function HomeScreen() {
     const fecthData = async () => {
       try {
         setLoading(true);
-        const { data } = await axios.get("/api/events");
+        const { data } = await Axios.get("/api/events");
         setLoading(false);
         setEvents(data);
       } catch (err) {
@@ -22,8 +23,53 @@ function HomeScreen() {
     };
     fecthData();
   }, []);
+
+  const EVENT_CREATE_REQUEST = "EVENT_CREATE_REQUEST";
+  const EVENT_CREATE_SUCCESS = "EVENT_CREATE_SUCCESS";
+  const EVENT_CREATE_FAIL = "EVENT_CREATE_FAIL";
+  const EVENT_CREATE_RESET = "EVENT_CREATE_RESET";
+
+  const createEvent = () => async (dispatch) => {
+    dispatch({ type: EVENT_CREATE_REQUEST });
+    try {
+      const { data } = await Axios.post("/api/events", {});
+      dispatch({
+        type: EVENT_CREATE_SUCCESS,
+        payload: data.event,
+      });
+    } catch (error) {
+      const message =
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message;
+      dispatch({ type: EVENT_CREATE_FAIL, payload: message });
+    }
+  };
+  const dispatch = useDispatch();
+  const eventCreate = useSelector((state) => state.eventCreate);
+  const {
+    loading: loadingCreate,
+    error: errorCreate,
+    success: successCreate,
+    event: createdEvent,
+  } = eventCreate;
+
+  useEffect(() => {
+    if (successCreate) {
+      dispatch({ type: EVENT_CREATE_RESET });
+      props.history.push(`/event/${createdEvent._id}/edit`);
+    }
+  }, [createdEvent, dispatch, props.history, successCreate]);
+  const createHandler = () => {
+    dispatch(createEvent());
+  };
   return (
-    <div>
+    <div className="row">
+      <button type="button" className="primary" onClick={createHandler}>
+        Create Event
+      </button>
+      {loadingCreate && <LoadingBox></LoadingBox>}
+      {errorCreate && <MessageBox variant="danger">{errorCreate}</MessageBox>}
       {loading ? (
         <LoadingBox></LoadingBox>
       ) : error ? (
